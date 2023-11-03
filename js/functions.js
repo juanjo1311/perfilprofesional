79bd13590b593e1137ac8b3bb975b027ee9d246c
juanjo1311 <jjlozanod@ufpso.edu.co>
2023-11-03 14:17:03 -0500

    cargando archivos

--- js/functions.js
+++ js/functions.js
@@ -70,3 +70,76 @@ console.log("division = "+divi.toFixed(2));
 // modulo
 var modulo = number_one % number_two;
 console.log("modulo = "+modulo);
+
+//ANDA && con if - Ampersand
+var bool = true;
+var numeric = 10;
+if(!bool && numeric === "10"){ //bool = true
+    console.log("ENTRA IF");
+}
+else{
+    console.log("ENTRA ELSE");
+}
+if(bool || numeric == 7 ){
+    console.log("ENTRA IF");
+    numeric++;
+}else{
+    console.log("ENTRA ELSE");
+    numeric--;
+}
+console.log(numeric);
+//for 
+
+for(let j = 0; j<array_text.length; j++){
+    console.log(array_text[j] + (j+1));
+}
+//WHILE
+let f=0;
+while(f<array_text.length){
+    console.log(array_text[f] + (f+1));
+    f++;
+}
+
+//DO WHILE
+let m=0;
+do{
+    console.log(array_text[m] + (m+1));
+    m++
+}while(m<array_text.length);
+
+//FUNCIONES Y EVENTOS
+function load_page(){
+    alert("LA PAGINA SE HA CARGADO CORRECTAMENTE");
+}
+
+function change_color(){
+    document.body.style.backgroundColor = "red";
+    document.body.style.color = "#fff";
+}
+
+const btn_limpiar = document.querySelector("#clear_color");
+clear_color-addEventListener("click", () =>{
+    document.body.style.backgroundColor = "#fff";
+    document.body.style.color = "#000";
+})
+
+// REGISTRO FORMULARIO
+const form_register = document.getElementById("form_register");
+const nombres       = document.getElementById("nombres");
+const apellidos     = document.getElementById("apellidos");
+const information   = document.getElementById("information");
+
+form_register.addEventListener("submit", name_event =>{
+    name_event.preventDefault();
+    let info = "";
+    //console.log(nombres);
+    if(nombres.ariaValueMax.length <= 2 || apellidos.ariaValueMax.lengt <=2){
+        info+="NOMBRES Y/O APELLIDOS INVALIDOS";
+        information.style.colo = "red";
+    }
+    else{
+        alert(nombres.value + " " + apellidos.value);
+    }
+    information.innerText = info;
+})
+//value almacena lo que ingreso en el input
